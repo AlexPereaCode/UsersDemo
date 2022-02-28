4ba216286bc001238469d10ea7ed44f94d382482
Alex Perea <alexpereacode@gmail.com>
2022-02-28 23:24:42 +0100

    Added xcode empty project

---
+++ UsersDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsersDemo
+//
+//  Created by Alejandro Perea Navarrete on 28/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsersDemo/UsersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersDemoApp.swift
+//  UsersDemo
+//
+//  Created by Alejandro Perea Navarrete on 28/2/22.
+//
+
+import SwiftUI
+
+@main
+struct UsersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
